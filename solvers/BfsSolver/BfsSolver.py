11b30940af61e8565d3e8e7c997e36d14e8eb87a
Tomek Zawadzki <tomekzawadzki98@gmail.com>
2020-06-12 09:07:41 +0200

    add path reconstruction

--- solvers/BfsSolver/BfsSolver.py
+++ solvers/BfsSolver/BfsSolver.py
@@ -1,4 +1,4 @@
-from typing import List
+from typing import List, Tuple, Optional
 
 import networkx as nx
 
@@ -6,6 +6,7 @@ from DataClasses.Transfer import Transfer
 from solvers.BfsSolver.BfsSolverData import BfsSolverData
 from solvers.ISolver import ISolver
 from DataClasses.TransferQuery import TransferQuery
+from utils import int_to_time
 
 
 class BfsSolver(ISolver):
@@ -46,9 +47,24 @@ class BfsSolver(ISolver):
         shortest_path = nx.shortest_path(self.data.G_B, source, target, 'weight')
 
         # step 5: reconstruct the result
-        sequence = []
+        changes = []
+        last_block_id = None
         for node in shortest_path:
-            print(node)
+            stop_id, time, block_id = node
+            if block_id != last_block_id:
+                last_block_id = block_id
+                changes.append(node)
+
+        sequence = []
+        for (start_stop_id, start_time, block_id), (end_stop_id, end_time, _) in zip(changes[::2], changes[1::2]):
+            route_number = 'MPK'  # TODO: retrieve line number
+            start_stop_name = self.data.stops_df.loc[start_stop_id]['stop_name']
+            end_stop_name = self.data.stops_df.loc[end_stop_id]['stop_name']
+            start_date = end_date = query.start_date  # TODO: retrieve start and end date
+            start_time = int_to_time(start_time)
+            end_time = int_to_time(end_time)
+            transfer = Transfer(route_number, start_stop_name, end_stop_name, start_date, start_time, end_date, end_time)
+            sequence.append(transfer)
 
         results = [sequence]
         return results
