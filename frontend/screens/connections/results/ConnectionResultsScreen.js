65eb4f39c1486a535ca5e617079237236422c43f
Tomek Zawadzki <tomekzawadzki98@gmail.com>
2020-11-11 12:04:20 +0100

    Move plot button to ConnectionResultsScreen

--- frontend/screens/connections/results/ConnectionResultsScreen.js
+++ frontend/screens/connections/results/ConnectionResultsScreen.js
@@ -1,8 +1,25 @@
 import * as React from "react";
+import { IconButton } from "react-native-paper";
 import ConnectionResults from "./ConnectionResults";
 
 export default function ConnectionResultsScreen({ navigation, route }) {
   const connections = route.params.connections;
+
+  const handleShowPlot = () => {
+    navigation.navigate("ConnectionResultsPlot", { connections });
+  };
+
+  React.useLayoutEffect(() => {
+    navigation.setOptions({
+      headerRight: () => (
+        <IconButton
+          icon="chart-bar"
+          onPress={handleShowPlot}
+          style={{ marginRight: 16 }}
+        />
+      ),
+    });
+  }, [navigation]);
 
   return (
     <ConnectionResults connections={connections} navigation={navigation} />
