42652e1338d7d22747044c2d7a4db95abc9d3e93
Tomek Zawadzki <tomekzawadzki98@gmail.com>
2020-11-14 12:04:05 +0100

    Fix bottom spacing in connection results

--- frontend/screens/connections/results/ConnectionResults.js
+++ frontend/screens/connections/results/ConnectionResults.js
@@ -1,19 +1,21 @@
 import * as React from "react";
-import { ScrollView } from "react-native";
+import { View, ScrollView } from "react-native";
 import ConnectionResult from "./ConnectionResult";
 
 export default function ConnectionResults({ connections, navigation }) {
   return (
-    <ScrollView style={{ padding: 16 }}>
-      {connections
-        .filter((connection) => !connection.walking_only) // TODO: display walking-only connection
-        .map((connection, i) => (
-          <ConnectionResult
-            connection={connection}
-            navigation={navigation}
-            key={i}
-          />
-        ))}
+    <ScrollView>
+      <View style={{ margin: 16 }}>
+        {connections
+          .filter((connection) => !connection.walking_only) // TODO: display walking-only connection
+          .map((connection, i) => (
+            <ConnectionResult
+              connection={connection}
+              navigation={navigation}
+              key={i}
+            />
+          ))}
+      </View>
     </ScrollView>
   );
 }
