c59aa1b11290fab0bd84078f9b0743e70496ff30
Mikołaj Tomalik <tomalikem@op.pl>
2020-11-16 17:15:16 +0100

    Walking edges 'returned

--- backend/src/examples/flask_examples/sample_queries.py
+++ backend/src/examples/flask_examples/sample_queries.py
@@ -3,6 +3,7 @@ from enum import Enum
 
 class ConnectionQuerySamples(Enum):
     krzeszowice_wolica = {"start_datetime": "2020-05-24 12:00:00", "start_stop_name": 'Krzeszowice Dworzec Autobusowy', "end_stop_name": 'Wolica Most'}
+    jubilat_kostrze = {"start_datetime": "2020-05-24 12:00:00", "start_stop_name": 'Jubilat', "end_stop_name": 'Wolica Most'}
     czarnowiejska_kleparz = {"start_datetime": "2020-05-24 12:00:00", "start_stop_name": 'Czarnowiejska', "end_stop_name": 'Nowy Kleparz'}
     grunwaldzkie_kosciuszki = {"start_datetime": "2020-05-24 12:00:00", "start_stop_name": 'Rondo Grunwaldzkie', "end_stop_name": 'Kopiec Kościuszki'}
     dunikowskoego_struga = {"start_datetime": "2020-05-24 12:00:00", "start_stop_name": 'Dunikowskiego', "end_stop_name": 'Struga'}
