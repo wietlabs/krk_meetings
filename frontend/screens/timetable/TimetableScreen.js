0a30cfb1b43bb372e354566739f3b6adb4ff2a62
Tomek Zawadzki <tomekzawadzki98@gmail.com>
2020-11-18 08:53:30 +0100

    Use onShouldStartLoadWithRequest instead of originWhitelist

--- frontend/screens/timetable/TimetableScreen.js
+++ frontend/screens/timetable/TimetableScreen.js
@@ -1,11 +1,13 @@
 import * as React from "react";
 import { WebView } from "react-native-webview";
 
+const url = "http://rozklady.ztp.krakow.pl/";
+
 export default function TimetableScreen() {
   return (
     <WebView
-      source={{ uri: "http://rozklady.ztp.krakow.pl/" }}
-      originWhitelist={["http://rozklady.ztp.krakow.pl"]}
+      source={{ uri: url }}
+      onShouldStartLoadWithRequest={(request) => request.url.startsWith(url)}
       style={{ backgroundColor: "white" }}
     />
   );
