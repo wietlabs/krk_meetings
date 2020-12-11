274327016435956381361c4fb84105c1a9b911e4
Tomek Zawadzki <tomekzawadzki98@gmail.com>
2020-12-11 11:41:45 +0100

    Move radio button group to a separate component

---
+++ frontend/components/SelectAccount.js
@@ -0,0 +1,29 @@
+import * as React from "react";
+import { View } from "react-native";
+import { List, RadioButton } from "react-native-paper";
+import { censorUuid } from "../utils";
+
+export default function SelectAccount({
+  users = [],
+  selected = null,
+  onChange = () => {},
+}) {
+  return (
+    <RadioButton.Group value={selected}>
+      {users.map(({ uuid, nickname }) => (
+        <List.Item
+          key={uuid}
+          onPress={() => onChange(uuid)}
+          title={nickname === null ? "Tożsamość bez nazwy" : nickname}
+          titleStyle={nickname === null ? { opacity: 0.2 } : null}
+          description={censorUuid(uuid)}
+          left={(props) => (
+            <View style={{ marginTop: 8 }}>
+              <RadioButton {...props} value={uuid} color="deepskyblue" />
+            </View>
+          )}
+        />
+      ))}
+    </RadioButton.Group>
+  );
+}
